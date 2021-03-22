3e723c6c009a62f090f7c5892fcd52565c87f429
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2021-03-22 21:50:12 +0300

    feat: add vue and main files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <SignUpForm />
+</template>
+
+<script>
+import SignUpForm from "@/components/SignUpForm";
+
+export default {
+  name: 'App',
+  components: {
+    SignUpForm
+  },
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background: #eee;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
